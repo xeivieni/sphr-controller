ae06c9da49e04f93657e695fde86463ec2de067a
xeivieni <clement.mondion@gmail.com>
2016-05-05 15:04:50 +0200

    animation improved
    Still to fix : fade in and out animations for the circles and the text

--- app/js/connector.js
+++ app/js/connector.js
@@ -2,57 +2,11 @@
  * Created by clem on 04/05/2016.
  */
 
-/*
-    // Wifi scan and connection if found
-child = exec('osx-wifi-cli', function (error, stdout, stderr) {
-    if (stdout !== null) {
-        var name = stdout.slice(21);
-        if (name.match(/Pi_AP/) == null) {
-            console.log("Not connected to a robot, searching one near...");
-            child = exec('osx-wifi-cli scan',
-                function (error, stdout, stderr) {
-                    if (stdout !== null) {
-                        var scan = stdout;
-                        if (scan.match(/Pi_AP/) == null) {
-                            console.log("No robot found");
-                            connectedToRobot = true;
-                        }
-                        else {
-                            console.log("Robot found, connecting...");
-                            child = exec('osx-wifi-cli Pi_AP raspberry',
-                                function (error, stdout, stderr) {
-                                    child = exec('osx-wifi-cli',
-                                        function (error, stdout, stderr) {
-                                            console.log("Connection succesful");
-                                            connectedToRobot = true;
-                                        });
-                                });
-                        }
-                    } else if (stderr !== null) {
-                        console.log('stderr: ' + stderr);
-                    } else if (error !== null) {
-                        console.log('exec error: ' + error);
-                    }
-                });
-        }
-        else {
-            console.log("Connected to a robot ! ");
-            connectedToRobot = true;
-        }
-    } else if (stderr !== null) {
-        console.log('stderr: ' + stderr);
-    } else if (error !== null) {
-        console.log('exec error: ' + error);
-    }
-});*/
-
-var exec = require('child_process').exec,
-    child;
 var wifi = require('wifi-control');
 
 var settings = {
     debug : false,
-    iface: 'wlan0',
+    iface: 'en0',
     connectionTimeout: 10000
 };
 
@@ -62,90 +16,24 @@ var Connector = (function () {
 
     self.connect = function(config, callback){
         wifi.connectToAP(config, function(err, response) {
-            if (err) console.log(err);
+            if (err) console.log(err); //GERER CA DANS LE CALLBACK ET AFFICHER UN MESSAGE D'ERREUR
             callback(response);
         });
     };
 
     self.scan = function(callback){
         wifi.scanForWiFi( function(err, response) {
-            if (err) console.log(err);
+            if (err) console.log(err); //GERER CA DANS LE CALLBACK ET AFFICHER UN MESSAGE D'ERREUR
             callback(response);
         });
     };
 
     self.getWifi = function(callback){
-        callback(wifi.getIfaceState());
+        var response = wifi.getIfaceState();
+        callback(response);
     };
 
     return self;
 })();
-//
-//var Connector = (function () {
-//    var self = {};
-//
-//    function isConnected(callback) {
-//        console.log('Looking to see if the actual wifi name is Pi-AP');
-//        child = exec('osx-wifi-cli', function (error, stdout) {
-//            if (stdout !== null) {
-//                var name = stdout.slice(21);
-//                if (name.match(/Pi_AP/) == null) {
-//                    console.log("No - Wifi network is not named Pi_AP.");
-//                    callback(false);
-//                } else {
-//                    console.log("Yes - Wifi network is named Pi_AP ! :)");
-//                    callback(true);
-//                }
-//            }
-//        });
-//    }
-//
-//    function searchRpi(callback) {
-//        console.log('Searching for a wifi named Pi_AP in the available wifi list...');
-//        child = exec('osx-wifi-cli scan', function (error, stdout) {
-//            if (stdout !== null) {
-//                if (stdout.match(/Pi_AP/) == null) {
-//                    console.log("No wifi named Pi_AP in the list.");
-//                    callback(false);
-//                } else {
-//                    console.log("Yes there is a wifi named Pi_AP in the list ! :)");
-//                    callback(true);
-//                }
-//            }
-//        });
-//    }
-//
-//    self.connectRpi = function (callback) {
-//        isConnected(ic => {
-//            if (ic === false) {
-//                console.log('not connected yet');
-//                searchRpi(ip => {
-//                    if (ip === true) {
-//                        console.log('found a robot');
-//                        child = exec('osx-wifi-cli Pi_AP coucuo', function (error, stdout, stderr) {
-//                            if (error !== null){
-//                                callback(undefined);
-//                            }
-//                            self.connectRpi(callback);
-//                        });
-//
-//                    } else {
-//                        console.log('no robot found');
-//                        callback(false);
-//                    }
-//                })
-//
-//            } else {
-//                console.log('already connected');
-//                callback(true);
-//                document.getElementsByClassName("")
-//            }
-//        });
-//
-//    };
-//
-//    return self;
-//
-//})();
 
 module.exports = Connector;
--- app/js/main.js
+++ app/js/main.js
@@ -7,86 +7,125 @@ var Connector = require('./js/connector');
 var instructions = document.getElementById('instructions');
 var waiter;
 
-var mode = "arrows";
-
-var endLoadAnimation = function (response) {
-    if (response === true) {
-        console.log('Yipikayé motherfucker');
-        instructions.removeChild(waiter);
-        var click = document.createElement("div");
-        click.innerHTML = "<span style='font-size: 40px'>Click to play</span><br/>";
-        var text;
-        if (mode == "leap") {
-            text = click.innerHTML + "Use leap motion to move the ball";
-        } else if (mode == "arrows") {
-            text = click.innerHTML + "Use arrows to move around";
-        } else {
-            text = click.innerHTML + "magical move";
-        }
-        click.innerHTML = text;
-        click.addEventListener('click', function (event) {
-            instructions.style.display = 'none';
-            controlsEnabled = true;
-        }, false);
-
-        instructions.appendChild(click);
-    } else if (response == false) {
-        console.log('Branche ton Rpi maggle');
-    } else {
-        console.log('Erreur de connexion')
-    }
-
+var config = {
+    ssid: 'Pi_AP',
+    password: 'raspberry'
 };
 
-var createClass = function (elementType, className, i, innerHtml) {
-    console.log("la on cree la classe", className);
+var messages = {
+    "check": ["#", "reading wifi network", ''],
+    "scan": ["#", "looking for ", "Pi_AP ", "in the available wifi network list"],
+    "connection": ["#", "connecting to ", "Pi_AP"],
+    "success": ["#", "connected"],
+    "error": ["#", "0 ", "robot found"],
+    "title": ["- Sphr ", "Motion -"]
+};
+
+var mode = "arrows";
+
+var appendWelcome = function(){
+    var container3 = createClass("div", "container3", "container container", 3, '');
+    var message2 = createClass("h1", "message2", "msg", '', '');
+    for (var k = 0; k< messages["title"].length; k++){
+        message2.appendChild(createClass("span", "span-color"+(k+2), "color", k+2, messages["title"][k]));
+    }
+    container3.appendChild(message2);
+    var globalContainer = document.getElementById("global");
+    globalContainer.appendChild(container3);
+};
+
+var updateMessage = function(messageId){
+    var text = document.getElementById("message1");
+    var textContainer = document.getElementById("container2");
+    var circlesContainer = document.getElementById("container1");
+    textContainer.removeChild(text);
+    var message = createClass("p", "message1", "msg", '', '');
+    for (var i = 0; i < messages[messageId].length; i++){
+        message.appendChild(createClass("span", "span-color"+(i+1), "color", i+1, messages[messageId][i]));
+    }
+    if (messageId === "success"){
+        textContainer.style.animationName = "fadeOutDown";
+        textContainer.style.animationDelay = "5s";
+        textContainer.style.animationDuration = "2s";
+        circlesContainer.style.animationName = "fadeOutDown";
+        circlesContainer.style.animationDelay = "5s";
+        circlesContainer.style.animationDuration = "2s";
+        appendWelcome();
+    }
+    textContainer.appendChild(message);
+};
+
+
+var createClass = function (elementType, id, className, i, innerHtml) {
     var newClass = document.createElement(elementType);
+    newClass.id = id;
     newClass.className = className + i;
     newClass.innerHTML = innerHtml;
     return newClass;
 };
 
 var createLoader = function (status) {
-    var messages = {
-        "check": ["#", "reading wifi network", ''],
-        "scan": ["#", "looking for", "Pi_AP", "in the available wifi network list"],
-        "connection": ["#", "connecting to", "Pi_AP"],
-        "success": ["#", "connected"],
-        "title": ["· Sphr", "Motion ·"]
-    };
-    var global = createClass("div", '', '', '');
-    var container = createClass("div", "container container", 1, '');
+    var global = createClass("div", "global", '', '', '');
+    var container = createClass("div", "container1", "container container", 1, '');
     var i;
     for (i = 1; i < 4; i++) {
-        container.appendChild(createClass("div", "circle circle", i + 1, ''));
+        container.appendChild(createClass("div", ("circle"+i), "circle circle", i, ''));
     }
-    var container2 = createClass("div", "container container", 2, '');
-    var message = createClass("p", "msg", '', '');
+    var container2 = createClass("div", "container2", "container container", 2, '');
+    var message = createClass("p", "message1", "msg", '', '');
     for (var j = 0; j< messages[status].length; j++){
-        message.appendChild(createClass("span", "color", j+1, messages[status][j]));
+        message.appendChild(createClass("span", "span-color"+(j+1), "color", j+1, messages[status][j]));
+    }
+    if (status === "success"){
+        container2.style.animationName = "fadeOutDown";
+        //container2.style.animationDelay = "5s";
+        container2.style.animationDuration = "2s";
+        container.style.animationName = "fadeOutDown";
+        //container.style.animationDelay = "5s";
+        container.style.animationDuration = "2s";
+        appendWelcome();
     }
     container2.appendChild(message);
-    var container3 = createClass("div", "container container", 3, '');
-    var message2 = createClass("h1", "msg", '', '');
-    for (var k = 0; k< messages["title"].length; k++){
-        message2.appendChild(createClass("span", "color", k+2, messages["title"][k]));
-    }
-    container3.appendChild(message2);
     global.appendChild(container);
     global.appendChild(container2);
-    global.appendChild(container3);
-    return global
+    return global;
+};
+
+var connectToWifi = function(response){
+    if (response['success'] === true){
+        updateMessage("success");
+    }
 };
 
 
+var readWifiList = function (response) {
+    var i, found = false;
+    for (i in response['networks']) {
+        if (response['networks'][i]['ssid'] === 'Pi_AP') {
+            found = true;
+            updateMessage("connection");
+            Connector.connect(config, connectToWifi);
+        }
+    }
+    if (found === false){
+        updateMessage("error");
+    }
+};
+
+var readWifiName = function(response){
+    if (response['ssid'] === 'Pi_AP' && response['connection'] === 'connected') {
+        updateMessage("success");
+    }
+    else {
+        updateMessage("scan");
+        Connector.scan(readWifiList);
+    }
+};
 
 var load = function () {
-    console.log("not connected, supposed to have an animation");
-    waiter = createLoader("connection");
+    waiter = createLoader("check");
     instructions.appendChild(waiter);
-    Connector.connectRpi(x => {
-        endLoadAnimation(x)
-    });
+    Connector.getWifi(readWifiName);
 };
 
 load();
